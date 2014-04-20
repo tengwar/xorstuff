0a844fa820e3eecda26ba2988a2fe6da9f4e56f7
Ganapati <ganapati@bobib.fr>
2014-04-20 10:44:36 +0200

    Move code into class

--- xorstuff.py
+++ xorstuff.py
@@ -13,52 +13,83 @@ from moduleBaseClass import ModuleBaseClass
 from guess_keylength import GuessKeyLength
 from operator import itemgetter
 
-list_types = {}
+class XorStuff:
 
-def loadFilesTypes(path):
-    list_types = {}
-    files = glob.glob(path + "*")
-    for file in files:
-        file_name, file_extension = os.path.splitext(file)
-        if not file_name.endswith("__init__") and file_extension == ".py":
-            module_name = file_name.replace("/", ".")
-            mod = __import__(module_name)
-            modules = module_name.split('.')
-            for module in modules[1:]:
-                mod = getattr(mod, module)
-                if issubclass(mod.Module, ModuleBaseClass):
-                    instance = mod.Module()
-                    list_types[instance.name] = instance
-    return list_types
+    def __init__(self, list_types={}):
+        self.list_types = list_types
 
-def xor(data, key, file_type=None):
-    result = []
-    for data, char_key in izip(data, cycle(key)):
-        byte = chr(ord(data) ^ ord(char_key))
-        if file_type is not None:
-            if not file_type.live_check(byte):
-                return None
-        result.append(byte)
-    return ''.join(result)
+    def loadFilesTypes(self, path):
+        list_types = {}
+        files = glob.glob(path + "*")
+        for file in files:
+            file_name, file_extension = os.path.splitext(file)
+            if not file_name.endswith("__init__") and file_extension == ".py":
+                module_name = file_name.replace("/", ".")
+                mod = __import__(module_name)
+                modules = module_name.split('.')
+                for module in modules[1:]:
+                    mod = getattr(mod, module)
+                    if issubclass(mod.Module, ModuleBaseClass):
+                        instance = mod.Module()
+                        list_types[instance.name] = instance
+        return list_types
 
-def getFileContent(filepath, length=None):
-    bin_file = ''
-    with open(filepath, "rb") as f:
-        byte = f.read(1)
-        index = 0
-        while byte != "":
-            bin_file = bin_file + byte
+    def xor(self, data, key, file_type=None):
+        result = []
+        for data, char_key in izip(data, cycle(key)):
+            byte = chr(ord(data) ^ ord(char_key))
+            if file_type is not None:
+                if not file_type.live_check(byte):
+                    return None
+            result.append(byte)
+        return ''.join(result)
+
+    def getFileContent(self, filepath, length=None):
+        bin_file = ''
+        with open(filepath, "rb") as f:
             byte = f.read(1)
-            if length is not None:
-                if index == length:
-                    break
-                index = index + 1
-    return bin_file
+            index = 0
+            while byte != "":
+                bin_file = bin_file + byte
+                byte = f.read(1)
+                if length is not None:
+                    if index == length:
+                        break
+                    index = index + 1
+        return bin_file
+
+    def get_pass(self, bin_file, file_type, key_length, grep=None):
+        header = file_type.header
+        # Padding of header with %s if key length > header length
+        if int(key_length) > len(header):
+            header = "%s%s" % (header, '%s' * (int(key_length) - len(header.replace('%s', '?'))))
+
+        bf_length = header.count('%s')
+        bin_header = self.getFileContent(args.filename, len(header.replace('%s', '?')))
+
+        charset = ''.join([chr(i) for i in range(128)])
+        key_charset = string.ascii_letters + string.digits + string.punctuation
+
+        # generate keys
+        for char in itertools.product(charset, repeat=bf_length):
+            generated_header = header % char
+            output = self.xor(bin_header, generated_header)
+            key = output[0: key_length]
+            if not [c for c in key if c not in key_charset]:
+                raw = self.xor(bin_file, key, file_type)
+                if raw is not None:
+                    if file_type.final_check(raw):
+                        if grep is not None:
+                            if grep in raw:
+                                yield key
+                        else:
+                            yield key
 
 if __name__ == "__main__":
 
     # Load modules
-    list_types = loadFilesTypes('modules/')
+    xor_stuff = XorStuff()
+    xor_stuff.list_types = xor_stuff.loadFilesTypes('modules/')
 
     parser = argparse.ArgumentParser(description='xor stuff with other stuff')
     parser.add_argument('-f', '--file', 
@@ -69,7 +100,7 @@ if __name__ == "__main__":
                         action="store",
                         dest='type',
                         help='filetype',
-                        choices=list_types.keys())
+                        choices=xor_stuff.list_types.keys())
     parser.add_argument('-l', '--length',
                         action="store",
                         dest="key_length",
@@ -89,22 +120,20 @@ if __name__ == "__main__":
 
     # xor with one key
     if args.xor_key is not None:
-        file = getFileContent(args.filename)
-        xored_data = xor(file, args.xor_key)
-        print xored_data
+        file = xorstuff.getFileContent(args.filename)
+        print  xor_stuff.xor(file, args.xor_key)
         exit(0)
     elif args.type is None or args.filename is None:
         print "Select file type (-t) and filename (-f)"
         exit(0)
 
+    # Open input file
     print "[*] Open file"
-    bin_file = getFileContent(args.filename)
-    file_type = list_types[args.type]
-    # select correct header
-    header = file_type.header
+    bin_file = xor_stuff.getFileContent(args.filename)
+    file_type = xor_stuff.list_types[args.type]
 
-    # pad for bruteforce
-    key_length = len(header)
+    # Search key length
+    key_length = len(file_type.header)
     if args.key_length is not None:
             key_length = int(args.key_length)
             fitnesses = [{'length': key_length, 'percents': 100}]
@@ -120,31 +149,10 @@ if __name__ == "__main__":
             print "    %s : %s%%" % (fitness['length'], fitness['percents'])
         print "[*] Most probable key length is %s*n" % divisors
 
+    # Search password
     for fitness in fitnesses:
         key_length = int(fitness['length'])
         print "[*] Key length set to %d" % key_length
 
-        # Padding of header with %s if key length > header length
-        if int(key_length) > len(header):
-            header = "%s%s" % (header, '%s' * (int(key_length) - len(header.replace('%s', '?'))))
-
-        bf_length = header.count('%s')
-        bin_header = getFileContent(args.filename, len(header.replace('%s', '?')))
-
-        charset = ''.join([chr(i) for i in range(128)])
-        key_charset = string.ascii_letters + string.digits + string.punctuation
-
-        # generate keys
-        for char in itertools.product(charset, repeat=bf_length):
-            generated_header = header % char
-            output = xor(bin_header, generated_header)
-            key = output[0: key_length]
-            if not [c for c in key if c not in key_charset]:
-                raw = xor(bin_file, key, file_type)
-                if raw is not None:
-                    if file_type.final_check(raw):
-                        if args.grep is not None:
-                            if args.grep in raw:
-                                print key
-                        else:
-                            print key
+        for password in xor_stuff.get_pass(bin_file, file_type, key_length, args.grep):
+            print password
